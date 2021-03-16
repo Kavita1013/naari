c8c696b26721110c5a1794e019844ae70dc16ed2
Kavita1013 <78211425+Kavita1013@users.noreply.github.com>
2021-03-16 20:21:23 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,54 @@
+// Options the user could type in
+const prompts = [
+  ["hi", "hey", "hello", "good morning", "good afternoon"],
+  ["how are you", "how is life", "how are things"],
+  ["how can i order the products"],
+  ["why should i buy online"],
+  ["how do create an account"],
+  
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what should i eat today"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["no","not sure","maybe","no thanks"],
+  [""],
+  ["haha","ha","lol","hehe","funny","joke"]
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  ["You can order easily using our online platform. You must first login. Once you find the product you need upon exploration, you can add it to cart and proceed to checkout."
+
+  ],
+  ["By ordering online you will you will  be able to go through order confirmation and payment process much faster.Ordering online on this platform in particular will support the businesses run by women. It is hassle-free and trackable."],
+  ["Go to login/signup and click “sign up”, then fill in all the required information and click “sign up”.  After submitting the form, your account will be confirmed and you will be notified."],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Sushi", "Pizza"],
+  ["Bro!"],
+  ["Great question"],
+  ["That's ok","I understand","What do you want to talk about?"],
+  ["Please say something :("],
+  ["Haha!","Good one!"]
+]
+
+// Random for any other user input
+
+const alternative = [
+  "Go on...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
